7c484de7e2c77ce6a950edab43383068a6079e25
tema <tema@vf8.ru>
2014-06-19 22:01:48 +0400

    данные для детального представления дисциплины добавлены.

--- data_for_desc.php
+++ data_for_desc.php
@@ -1,6 +1,9 @@
 <?php
+
+$id = 3;
+
 // получаем из базы нужный контент
-$sql = "SELECT * FROM kafedra, disciplines";
+$sql = "SELECT * FROM kafedra, disciplines WHERE kafedra.kaf_id = disciplines.kaf_fkey_id and disciplines.id = $id";
 $con = mysql_connect('localhost', 'root', '1234');
 mysql_set_charset( 'utf8' ); // кодировка в которой с базой работаем
 mysql_select_db('study', $con);
@@ -14,15 +17,10 @@ while($r = mysql_fetch_assoc($result)) {
 
 // преобразовываем информацию от СУБД в удобный для верстки формат
 $array = [];
-foreach ($rows as &$value) {
-	if ($value[kaf_id] == $value[kaf_fkey_id])
-	{
-		$kaf = &$value[kaf_name];
-		$disc = &$value[disc_name];
-		$id = &$value[id];
-		$array[$kaf][$id] = $disc;
-	}
-}
+$array[name] = $rows[0][disc_name];
+$array[kaf_name] = $rows[0][kaf_name];
+$array[alias] = $rows[0][disc_alias];
+$array[id] = $rows[0][id];
 
 // выводим результат
 var_dump($array);
