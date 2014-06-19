0f52b1e72c200d98a53eacc5a820f020ba7940c4
tema <tema@vf8.ru>
2014-06-19 23:53:53 +0400

    update desc

--- sql.php
+++ sql.php
@@ -1,5 +1,5 @@
 <?php
-
+// подключаемся к базе и выполняем SQL запрос
 function useSQL($sql) {
 	$con = mysql_connect('localhost', 'root', '1234');
 	mysql_set_charset( 'utf8' ); // кодировка в которой с базой работаем
@@ -8,6 +8,7 @@ function useSQL($sql) {
 	return $result;
 }
 
+// получаем данные для главной страницы
 function getMainData() {
 	// получаем из базы нужный контент
 	$sql = "SELECT * FROM kafedra, desciplines";
@@ -33,7 +34,7 @@ function getMainData() {
 	return $array;
 }
 
-// Получаем детальные данные о дисциплине
+// получаем детальные данные о дисциплине
 function getDescById($id) {
 	// получаем из базы нужный контент
 	$sql = "SELECT * FROM kafedra, desciplines WHERE kafedra.kaf_id = desciplines.kaf_fkey_id and desciplines.id = $id";
@@ -51,15 +52,22 @@ function getDescById($id) {
 	$array[kaf_name] = $rows[0][kaf_name];
 	$array[alias] = $rows[0][desc_alias];
 	$array[id] = $rows[0][id];
-
     return $array;
 }
 
+// обновляем данные о дисциплине
+function updateDesc($id, $name, $alias)
+{
+	$sql = "UPDATE desciplines SET desc_name = '$name', desc_alias = '$alias' WHERE id = '$id'";
+	$result = useSQL($sql);
+	return $result; // true or false
+}
+
+// updateDesc(1, 'test2', 'tst2');
 // $res = getMainData();
 // $id = 4;
 // $res = getDescById($id);
-
 // выводим результат
 // var_dump($res);
-// header( 'Content-Type: text/html; charset=utf-8' );
+// header('Content-Type: text/html; charset=utf-8');
 ?>
