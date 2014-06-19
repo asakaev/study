b02beac582d9093b001ac6456c1f879176f3fc41
tema <tema@vf8.ru>
2014-06-19 20:19:14 +0400

    sql nice

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
-echo 'test2';
+$sql = "SELECT * FROM kafedra, disciplines WHERE kafedra.id = disciplines.kafedra_id and kafedra.id = 1";
+$con = mysql_connect('localhost', 'root', '1234');
+mysql_select_db('study', $con);
 
+$result = mysql_query($sql);
+
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+
+var_dump($rows);
+
+// header('Content-Type: application/json');
+// echo json_encode($rows);
 
 ?>
