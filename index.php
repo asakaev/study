d90df64ede6b9d2c4a1c7e7c5d6b088d114bce23
tema <tema@vf8.ru>
2014-06-19 21:10:47 +0400

    Преобразования данных из базы в удобные для представления.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-$sql = "SELECT * FROM kafedra, disciplines WHERE kafedra.id = disciplines.kafedra_id and kafedra.id = 1";
+// получаем из базы нужный контент
+$sql = "SELECT * FROM kafedra, disciplines";
 $con = mysql_connect('localhost', 'root', '1234');
+mysql_set_charset( 'utf8' );
 mysql_select_db('study', $con);
 
 $result = mysql_query($sql);
@@ -11,9 +13,27 @@ while($r = mysql_fetch_assoc($result)) {
     $rows[] = $r;
 }
 
-var_dump($rows);
+// var_dump($rows);
+
+
+// преобразовываем информацию от СУБД в удобный для верстки формат
+$array = [];
+foreach ($rows as &$value) {
+	if ($value[kaf_id] == $value[kaf_fkey_id])
+	{
+		$kaf = $value[kaf_name];
+		$disc = $value[disc_name];
+		$array[$kaf][] = $disc;
+	}
+}
+
+var_dump($array);
+
+
+
+header( 'Content-Type: text/html; charset=utf-8' );
 
 // header('Content-Type: application/json');
-// echo json_encode($rows);
+// echo json_encode($array);
 
 ?>
